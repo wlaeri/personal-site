41fe91a36fd986b128196637ca628512ee3cb3cf
Will Laeri <wlaeri@gmail.com>
2022-04-15 08:02:16 -0400

    added google tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,34 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { NextStrictCSP } from 'next-strict-csp'
 import { theme } from '../components/theme'
+
+const GOOGLE_TAG_MANAGER_ID = 'GTM-56JWM9X'
+
+const googleTagManagerScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','${GOOGLE_TAG_MANAGER_ID}');`
+
+// Hash scripts for strict content security policy
+NextStrictCSP.inlineJs = [googleTagManagerScript]
+const HeadCSP = process.env.NODE_ENV === 'production' ? NextStrictCSP : Head
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head>
+        <HeadCSP>
+          {process.env.NODE_ENV === 'production' && (
+            <meta httpEquiv="Content-Security-Policy" />
+          )}
+          {process.env.NODE_ENV === 'production' && (
+            <script
+              dangerouslySetInnerHTML={{
+                __html: googleTagManagerScript,
+              }}
+            />
+          )}
           <link rel="manifest" href="/manifest.json" />
           <meta name="theme-color" content={theme.colors.background} />
           <link rel="apple-touch-icon" href="/pwa/maskable_icon.png" />
@@ -30,8 +53,15 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Montserrat&display=optional"
             rel="stylesheet"
           />
-        </Head>
+        </HeadCSP>
         <body>
+          {process.env.NODE_ENV === 'production' && (
+            <noscript
+              dangerouslySetInnerHTML={{
+                __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GOOGLE_TAG_MANAGER_ID}" height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+              }}
+            />
+          )}
           <Main />
           <NextScript />
         </body>
